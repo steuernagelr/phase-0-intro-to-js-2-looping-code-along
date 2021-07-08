bb1d8ec45b2f3ed2722ba2b8addd97ecafb56a06
Rielly Steuernagel <reiguy@comcast.net>
2021-07-08 09:15:53 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solutions in this file
+//for (let age =30; age < 40; age++) {
+  //  console.log(`I'm ${age} years old. Happy Birthday to me!`);
+//}
+
+//const gifts = ["teddy bear", "drone", "doll"];
+
+//function wrapGifts(gifts) {
+//    for (let i = 0; i < gifts.length; i++) {
+//    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+//}
+
+//    return gifts;
+//}
+
+wrapGifts(gifts);
+
+function writeCards(names, event) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    return messages
+}
+
+
+function countDown(positiveNumber) {
+    while (positiveNumber >= 0) {
+        console.log(positiveNumber);
+        positiveNumber--;
+    }
+
+}
